18992a8d966e4bc0df5a1880cad2e3b3fbc977b7
wilbert <weizhaowu@outlook.com>
2016-05-06 20:20:30 +0800

    add DiskLrucache

--- app/src/main/java/com/wilbert/sysusdcs/activity/MainActivity.java
+++ app/src/main/java/com/wilbert/sysusdcs/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.wilbert.sysusdcs.activity;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.util.LruCache;
@@ -12,18 +15,27 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.wilbert.sysusdcs.R;
+import com.wilbert.sysusdcs.network.DiskLruCache;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.Buffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends BaseActivity {
     public static final int GET_BITMAP = 1234;
     public static final String BITMAP_KEY = "bitmap";
+    private static final int DISK_CACHE_INDEX = 0;
+    private static final int IO_BUFFER_SIZE = 8*1024;
     int maxMemory = (int) (Runtime.getRuntime().maxMemory()/1024);
     int cacheSize = maxMemory / 8;
     LruCache<String, Bitmap> mMemryCache = new LruCache<String, Bitmap>(cacheSize) {
@@ -33,9 +45,12 @@ public class MainActivity extends BaseActivity {
         }
     };
 
+    DiskLruCache diskLruCache;
+
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
+            Log.d("w", "set the bitmap");
             Bitmap bitmap = (Bitmap) msg.obj;
             imageView.setImageBitmap(bitmap);
             }
@@ -59,7 +74,15 @@ public class MainActivity extends BaseActivity {
 
     @Override
     void initData() {
-
+        File file = getDiskCacheDir(this.getApplicationContext(), "bitmap");
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        try {
+            diskLruCache = DiskLruCache.open(file, 1,1, 1024*1024*10);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -73,11 +96,20 @@ public class MainActivity extends BaseActivity {
                         Bitmap bitmap = null;
 
                         String uriString = "http://b.hiphotos.baidu.com/zhidao/pic/item/a6efce1b9d16fdfafee0cfb5b68f8c5495ee7bd8.jpg";
-                        Log.d("w", "hashkey");
                         bitmap = mMemryCache.get(hashKeyFormUrl(uriString));
                         if (bitmap == null) {
-                            bitmap = downloadBitmapFromUri(uriString);
-                            Log.d("w", "from uriString");
+                            try {
+                                bitmap = getBitmapFromDisk(uriString);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        if (bitmap == null) {
+                            try {
+                                bitmap = downloadBitmapFromUri(uriString);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                             handler.obtainMessage(1, bitmap).sendToTarget();
                         }
                 }});
@@ -85,17 +117,67 @@ public class MainActivity extends BaseActivity {
             }
         }
 
-    private Bitmap downloadBitmapFromUri(String uriString) {
+    private Bitmap getBitmapFromDisk(String uriString) throws IOException {
+        Bitmap bitmap = null;
+        String key = hashKeyFormUrl(uriString);
+        Log.d("w", "from disk");
+        DiskLruCache.Snapshot snapshot = diskLruCache.get(key);
+        if (snapshot != null) {
+            Log.d("w", "snapshot is not null");
+            InputStream fileInputStream = snapshot.getInputStream(0);
+            bitmap = BitmapFactory.decodeStream(fileInputStream);
+            if (bitmap == null) {
+                Log.d("w", "stream could not be decoded");
+            }
+            if (bitmap != null) {
+                Log.d("w", "bitmap is not null");
+                mMemryCache.put(key, bitmap);
+            }
+        }
+        return bitmap;
+    }
+
+    private Bitmap downloadBitmapFromUri(String uriString) throws IOException {
         Bitmap bitmap = null;
         HttpURLConnection urlConnection = null;
         BufferedInputStream in = null;
-
+        BufferedOutputStream out = null;
+        OutputStream outputStream = null;
         try {
+            Log.d("w", "from uri");
             final URL url = new URL(uriString);
             urlConnection = (HttpURLConnection) url.openConnection();
-            in = new BufferedInputStream(urlConnection.getInputStream());
+            in = new BufferedInputStream(urlConnection.getInputStream(), 8*1024);
+
+
+/*
             bitmap = BitmapFactory.decodeStream(in);
             mMemryCache.put(hashKeyFormUrl(uriString), bitmap);
+*/
+
+
+            DiskLruCache.Editor editor = diskLruCache.edit(hashKeyFormUrl(uriString));
+            if (editor != null) {
+                outputStream = editor.newOutputStream(0);
+                if (downloadUrlToStream(uriString, outputStream)) {
+                    Log.d("w", "download url to stream true");
+                    editor.commit();
+                } else {
+                    Log.d("w", "down load url to stream false");
+                    editor.abort();
+                }
+                diskLruCache.flush();
+            }
+/*
+            out = new BufferedOutputStream(outputStream, 8*1024);
+            int b;
+            while ((b = in.read()) != -1) {
+                out.write(b);
+            }
+            editor.commit();
+            diskLruCache.flush();
+*/
+            bitmap = getBitmapFromDisk(uriString);
         } catch (final IOException e) {
             Log.e("w", "Error in downloadBitmap: " + e);
         } finally {
@@ -111,6 +193,56 @@ public class MainActivity extends BaseActivity {
             }
         }
         return bitmap;
+    /*--------------------------------------------------------------------------*/
+/*
+        if (diskLruCache == null) {
+            return null;
+        }
+
+        String key = hashKeyFormUrl(uriString);
+        DiskLruCache.Editor editor = diskLruCache.edit(key);
+        if (editor != null) {
+            OutputStream outputStream = editor.newOutputStream(DISK_CACHE_INDEX);
+            if (downloadUrlToStream(uriString, outputStream)) {
+                Log.d("w", "downloadurltostream true");
+                editor.commit();
+            } else {
+                Log.d("w", "downloadurltostream false");
+                editor.abort();
+            }
+            diskLruCache.flush();
+        }
+        return getBitmapFromDisk(uriString);
+*/
+    }
+
+    private boolean downloadUrlToStream(String uriString, OutputStream outputStream) throws IOException {
+        HttpURLConnection urlConnection = null;
+        BufferedOutputStream out = null;
+        BufferedInputStream in = null;
+
+        try {
+            final URL url = new URL(uriString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream(),
+                    IO_BUFFER_SIZE);
+            out = new BufferedOutputStream(outputStream, IO_BUFFER_SIZE);
+
+            int b;
+            while ((b = in.read()) != -1) {
+                out.write(b);
+            }
+            return true;
+        } catch (IOException e) {
+            Log.e("w", "downloadBitmap failed." + e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            out.close();
+            in.close();
+        }
+        return false;
     }
 
     @Override
@@ -141,5 +273,16 @@ public class MainActivity extends BaseActivity {
         }
         return sb.toString();
     }
+    public File getDiskCacheDir(Context context, String uniqueName) {
+        boolean externalStorageAvailable = Environment
+                .getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        final String cachePath;
+        if (externalStorageAvailable) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
 
+        return new File(cachePath + File.separator + uniqueName);
+    }
 }
